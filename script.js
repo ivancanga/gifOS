342d1c59ca10bcb919025128cebea55e76b10fdf
ivancanga <55848546+ivancanga@users.noreply.github.com>
2019-12-13 20:57:47 -0300

    suggested gifs refactoring

--- script.js
+++ script.js
@@ -25,9 +25,33 @@ function suggestedGifs(gif) {
             return response.json()
         }).then(data => {
             console.log(data.data);
-            $("#gif_suggested").append("<div class='gif-box'><section class='gif-title-card'><p>#"+gif+"</p><img src='/gifOS/images/button3.svg' /></section><img class='gif-img' src=' " + data.data[0].images.original.url + " ' /><span class='btn-gif'><a href=' "+ data.data[0].bitly_url +"' target='_blank'>Ver más...</span><a></div>");
+            $("#gif_suggested").append("<div class='gif-box'><section class='gif-title-card'><p>#" + gif + "</p><img src='/gifOS/images/button3.svg' /></section><img class='gif-img' src=' " + data.data[0].images.original.url + " ' /><span class='btn-gif'><a href=' " + data.data[0].bitly_url + "' target='_blank'>Ver más...</span><a></div>");
         });
 }
+
+let myArray = ['SakuraCardCaptor','SailorMoon','Togepi','Gatomon','Lapras','Charmander','Sasuke','Squirtle']
+let rand = function(){
+    myArraynew = [];
+    let compara = myArray[Math.floor(Math.random() * myArray.length)];
+    i = 0;
+    while(!myArraynew.includes(compara) && i<4){
+        myArraynew.push(compara);
+        compara = myArray[Math.floor(Math.random() * myArray.length)];
+        i++;
+    }
+    return myArraynew;
+}
+
+console.log(rand()[0]);
+
+// if ($(window).width() <= 500) {
+//     $(document).ready(suggestedGifs(rand()));
+// } else {
+//     for(var i = 0; i < 4 ; i++){
+//         $(document).ready(suggestedGifs(rand()));
+//     }
+// }
+
 
 
 function getSearchResults() {
@@ -51,10 +75,10 @@ function getSearchResults() {
     return found
 }
 
-function resultadoSugerido(){
+function resultadoSugerido() {
     $(".autocomplete-content").css("display", "block");
     search = document.getElementById("search").value;
-    if(search === ""){
+    if (search === "") {
         $(".autocomplete-content").css("display", "none");
     }
     fetch('//api.giphy.com/v1/gifs/search?q=' + search +
@@ -65,9 +89,9 @@ function resultadoSugerido(){
             $("#sugerido1").html("");
             $("#sugerido2").html("");
             $("#sugerido3").html("");
-            $("#sugerido1").append("<div><a href='"+ data.data[0].bitly_url +"' target='_blank'>" + data.data[0].title + "</a></div>");
-            $("#sugerido2").append("<div><a href='"+ data.data[1].bitly_url +"' target='_blank'>" + data.data[1].title + "</a></div>");
-            $("#sugerido3").append("<div><a href='"+ data.data[2].bitly_url +"' target='_blank'>" + data.data[2].title + "</a></div>");
+            $("#sugerido1").append("<div><a href='" + data.data[0].bitly_url + "' target='_blank'>" + data.data[0].title + "</a></div>");
+            $("#sugerido2").append("<div><a href='" + data.data[1].bitly_url + "' target='_blank'>" + data.data[1].title + "</a></div>");
+            $("#sugerido3").append("<div><a href='" + data.data[2].bitly_url + "' target='_blank'>" + data.data[2].title + "</a></div>");
         });
 }
 
