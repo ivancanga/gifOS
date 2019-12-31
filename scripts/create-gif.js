861688174e7c3acb74d13236cf14463c08379a36
Iván Canga <ivancanga@outlook.com>
2019-12-31 14:45:37 -0300

    Create gif recording is working now! :D

--- scripts/create-gif.js
+++ scripts/create-gif.js
@@ -1,14 +1,53 @@
 var video = document.querySelector("#videoElement");
 
-function getStreamAndRecord() {
+async function getStream() {
     $(".create-gif-section").css("display", "none");
     $(".video-recording").css("display", "block");
-    navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: true })
-        .then(function (stream) {
-            video.srcObject = stream;
+    let stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
+    video.srcObject = stream;
+}
+
+function startRecording() {
+    let stream = video.srcObject;
+    recorder = RecordRTC(stream, {
+        type: 'video/webm',
+        frameRate: 1,
+        quality: 10,
+        onGifRecordingStarted: function () {
+            console.log('started')
+        }
+    });
+    recorder.startRecording();
+
+    document.getElementById('titleBox').innerHTML = "Capturando Tu Guifo";
+
+}
+
+function stopRecording() {
+    let stream = video.srcObject;
+    video.srcObject = null;
+    recorder.stopRecording(function () {
+        let blob = recorder.getBlob();
+        video.src = URL.createObjectURL(blob);
+        video.loop = true;
+        video.controls = true;
+        document.getElementById('titleBox').innerHTML = "Vista Previa";
+
+        let formData = new FormData();
+        formData.append('file', blob, 'myGif.gif');
+    });
+    stream.getTracks().forEach(function(track) {
+        track.stop();
+    });
+}
+
+function uploadGif() {
+    var miInit = {
+        method: 'POST',
+        mode: 'no-cors',
+    };
+    fetch('//upload.giphy.com/v1/gifs?api_key=xBWsI1LWcGLChS6L9d5ucODsG0BfkNEx', miInit)
+        .then(response => {
+            console.log(response);
         })
-        .then(function (record) {
-        });
 }
