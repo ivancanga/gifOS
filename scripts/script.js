0afd2e80887c1801b42a0ac64aaefddfc1558490
ivancanga <55848546+ivancanga@users.noreply.github.com>
2020-01-07 16:30:50 -0300

    Theme selected stills in LS and works good! ;)

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,8 @@
+let sailor_day = '/gifOS/styles/sailor_day.css';
+let sailor_night = '/gifOS/styles/sailor_night.css';
+
+// REFACTORIZAR TODO ESTO
+
 function ver() {
     document.getElementById("style-list").style.display = "block";
 };
@@ -7,31 +12,50 @@ function ocultar() {
 };
 
 function active_sailor_night() {
-    document.getElementById("theme").href = "/gifOS/styles/sailor_night.css";
+    document.getElementById("theme").href = sailor_night;
     document.getElementById("img-header").src = "/gifOS/images/gifOF_logo_dark.png";
     document.getElementById("lupa").src = "/gifOS/images/lupa.svg";
+    setThemeLS();
 }
 
 function active_sailor_day() {
-    document.getElementById("theme").href = "/gifOS/styles/sailor_day.css";
+    document.getElementById("theme").href = sailor_day;
     document.getElementById("img-header").src = "/gifOS/images/gifOF_logo.png";
     document.getElementById("lupa").src = "/gifOS/images/lupa_inactive.svg";
+    setThemeLS();
 }
 
 function changeLupa() {
     document.getElementById("lupa").src = "/gifOS/images/lupa.svg"
 }
 
-function showLsItems(){
-    document.getElementById("gif-count").innerHTML = `(${(localStorage.length)})`;
+function showLsItems() {
+    if (localStorage.length > 1) {
+        document.getElementById("gif-count").innerHTML = `(${(localStorage.length) - 1})`;
+    }
 }
 
-window.onload = function() {
+function setThemeLS() {
+    theme = document.getElementById("theme").getAttribute('href');
+    if (theme == sailor_day) {
+        localStorage.setItem('theme', 1);
+    } else {
+        localStorage.setItem('theme', 2);
+    }
+}
+
+window.onload = function () {
+
+    // Si alguna vez se predefinió el tema oscuro, permanece el tema por el value en LS
+    if (localStorage.getItem('theme') == 2) {
+        document.getElementById("theme").href = '/gifOS/styles/sailor_night.css';
+    }
 
     $("#rnd").append(Math.floor(Math.random() * 10000000));
 
     trendingGifs();
     showLsItems();
+    setThemeLS();
 
     $(function () {
         $(".search-bar input").keypress(function (e) {
