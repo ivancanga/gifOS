a906cfa234e72051b8be7f3201327ca2119d137f
Iván Canga <ivancanga@outlook.com>
2020-01-09 23:41:44 -0300

    Code format by Prettier

--- scripts/create-gif.js
+++ scripts/create-gif.js
@@ -4,121 +4,128 @@ var video = document.querySelector("#videoElement");
 let recording = false;
 
 async function getStream() {
-    $(".create-gif-section").css("display", "none");
-    $(".video-recording").css("display", "block");
-    let stream = await navigator.mediaDevices.getUserMedia({ audio: false, video: true });
-    video.srcObject = stream;
+  $(".create-gif-section").css("display", "none");
+  $(".video-recording").css("display", "block");
+  let stream = await navigator.mediaDevices.getUserMedia({
+    audio: false,
+    video: true
+  });
+  video.srcObject = stream;
 }
 
 function startRecording() {
-    recording = true;
-    recorder = RecordRTC(video.srcObject, {
-        type: 'gif',
-        frameRate: 1,
-        quality: 10,
-        width: 720,
-        onGifRecordingStarted: function () {
-            console.log('started')
-        }
-    });
+  recording = true;
+  recorder = RecordRTC(video.srcObject, {
+    type: "gif",
+    frameRate: 1,
+    quality: 10,
+    width: 720,
+    onGifRecordingStarted: function() {
+      console.log("started");
+    }
+  });
 
-    recorder.startRecording();
-    getDuration();
+  recorder.startRecording();
+  getDuration();
 
-    document.getElementById('titleBox').innerHTML = "Capturando Tu Guifo";
-    $(".confirm").css("display", "none");
-    $(".stop").css("display", "block");
+  document.getElementById("titleBox").innerHTML = "Capturando Tu Guifo";
+  $(".confirm").css("display", "none");
+  $(".stop").css("display", "block");
 }
 
 function stopRecording() {
-    video.srcObject.getTracks().forEach(function (track) {
-        track.stop();
+  video.srcObject.getTracks().forEach(function(track) {
+    track.stop();
+  });
+  recorder.stopRecording(function() {
+    recording = false;
+    // Se oculta video y muestra el preview del gif
+    video.style.display = "none";
+    document.querySelector(".gif-preview-container").style.display = "block";
+    preview = document.getElementById("gif-preview");
+    preview.src = URL.createObjectURL(recorder.getBlob());
+    document.getElementById("titleBox").innerHTML = "Vista Previa";
+
+    // Aca van los add para la barra de buffer dinámica
+
+    // ****
+
+    // Creamos el formulario para enviarlo por el body a giphy
+    let form = new FormData();
+    form.append("file", recorder.getBlob(), "myGif.gif");
+
+    document.getElementById("upload").addEventListener("click", () => {
+      uploadGif(form);
     });
-    recorder.stopRecording(function () {
-        recording = false;
-        // Se oculta video y muestra el preview del gif
-        video.style.display = 'none';
-        document.querySelector('.gif-preview-container').style.display = 'block';
-        preview = document.getElementById('gif-preview');
-        preview.src = URL.createObjectURL(recorder.getBlob());
-        document.getElementById('titleBox').innerHTML = "Vista Previa";
+  });
 
-        // Aca van los add para la barra de buffer dinámica
-        
-
-
-        // ****
-
-        // Creamos el formulario para enviarlo por el body a giphy
-        let form = new FormData();
-        form.append('file', recorder.getBlob(), 'myGif.gif');
-
-        document.getElementById('upload').addEventListener('click', () => {
-            uploadGif(form);
-        });
-
-    });
-
-
-    $(".stop").css("display", "none");
-    $(".btns-upload-gif").css("display", "flex");
+  $(".stop").css("display", "none");
+  $(".btns-upload-gif").css("display", "flex");
 }
 
 function uploadGif(gif) {
-    fetch('https://upload.giphy.com/v1/gifs?api_key=xBWsI1LWcGLChS6L9d5ucODsG0BfkNEx', {
-        method: 'POST',
-        body: gif,
+  fetch(
+    "https://upload.giphy.com/v1/gifs?api_key=xBWsI1LWcGLChS6L9d5ucODsG0BfkNEx",
+    {
+      method: "POST",
+      body: gif
+    }
+  )
+    .then(response => {
+      console.log(response.status);
+      return response.json();
     })
+    .then(data => {
+      console.log("Gif subido!");
+      fetch(
+        `https://api.giphy.com/v1/gifs/${data.data.id}?&api_key=xBWsI1LWcGLChS6L9d5ucODsG0BfkNEx`
+      )
         .then(response => {
-            console.log(response.status);
-            return response.json();
+          return response.json();
         })
         .then(data => {
-            console.log('Gif subido!');
-            fetch(`https://api.giphy.com/v1/gifs/${data.data.id}?&api_key=xBWsI1LWcGLChS6L9d5ucODsG0BfkNEx`)
-                .then(response => {
-                    return response.json();
-                })
-                .then(data => {
-                    localStorage.setItem(`gif-${data.data.id}`, JSON.stringify(data.data));
-                })
-        })
+          localStorage.setItem(
+            `gif-${data.data.id}`,
+            JSON.stringify(data.data)
+          );
+        });
+    });
 }
-
 
 // Entender como funciona esto bien
 function getDuration() {
-    let seconds = 0;
-    let minutes = 0;
-    let timer = setInterval(() => {
-        if (recording) {
-            if (seconds < 60) {
-                if (seconds <= 9) {
-                    seconds = '0' + seconds;
-                }
-                document.getElementById('timer').style.display = 'block';
-                document.getElementById('timer').innerHTML = `00:00:0${minutes}:${seconds}`;
-                seconds++;
-            } else {
-                minutes++;
-                seconds = 0;
-            }
+  let seconds = 0;
+  let minutes = 0;
+  let timer = setInterval(() => {
+    if (recording) {
+      if (seconds < 60) {
+        if (seconds <= 9) {
+          seconds = "0" + seconds;
         }
-        else {
-            clearInterval(timer)
-        }
-    }, 1000);
+        document.getElementById("timer").style.display = "block";
+        document.getElementById(
+          "timer"
+        ).innerHTML = `00:00:0${minutes}:${seconds}`;
+        seconds++;
+      } else {
+        minutes++;
+        seconds = 0;
+      }
+    } else {
+      clearInterval(timer);
+    }
+  }, 1000);
 }
 
 // Anima la barra de subida, entender como funciona esto bien
 let counter = 0;
-function animateProgressBar (bar) {
-    setInterval(() => {
-      if (counter < bar.length) {
-        bar.item(counter).classList.toggle('progress-bar-item-active')
-        counter++;
-      } else {
-        counter = 0;
-      }
-    }, 200)
-} 
+function animateProgressBar(bar) {
+  setInterval(() => {
+    if (counter < bar.length) {
+      bar.item(counter).classList.toggle("progress-bar-item-active");
+      counter++;
+    } else {
+      counter = 0;
+    }
+  }, 200);
+}
--- scripts/script.js
+++ scripts/script.js
@@ -1,72 +1,73 @@
-let sailor_day = '/gifOS/styles/sailor_day.css';
-let sailor_night = '/gifOS/styles/sailor_night.css';
+let sailor_day = "/gifOS/styles/sailor_day.css";
+let sailor_night = "/gifOS/styles/sailor_night.css";
 
 // REFACTORIZAR TODO ESTO
 
 function ver() {
-    document.getElementById("style-list").style.display = "block";
-};
+  document.getElementById("style-list").style.display = "block";
+}
 
 function ocultar() {
-    document.getElementById("style-list").style.display = "none";
-};
+  document.getElementById("style-list").style.display = "none";
+}
 
 function active_sailor_night() {
-    document.getElementById("theme").href = sailor_night;
-    document.getElementById("img-header").src = "/gifOS/images/gifOF_logo_dark.png";
-    document.getElementById("lupa").src = "/gifOS/images/lupa.svg";
-    setThemeLS();
+  document.getElementById("theme").href = sailor_night;
+  document.getElementById("img-header").src =
+    "/gifOS/images/gifOF_logo_dark.png";
+  document.getElementById("lupa").src = "/gifOS/images/lupa.svg";
+  setThemeLS();
 }
 
 function active_sailor_day() {
-    document.getElementById("theme").href = sailor_day;
-    document.getElementById("img-header").src = "/gifOS/images/gifOF_logo.png";
-    document.getElementById("lupa").src = "/gifOS/images/lupa_inactive.svg";
-    setThemeLS();
+  document.getElementById("theme").href = sailor_day;
+  document.getElementById("img-header").src = "/gifOS/images/gifOF_logo.png";
+  document.getElementById("lupa").src = "/gifOS/images/lupa_inactive.svg";
+  setThemeLS();
 }
 
 function changeLupa() {
-    document.getElementById("lupa").src = "/gifOS/images/lupa.svg"
+  document.getElementById("lupa").src = "/gifOS/images/lupa.svg";
 }
 
 function showLsItems() {
-    if (localStorage.length > 1) {
-        document.getElementById("gif-count").innerHTML = `(${(localStorage.length) - 1})`;
-    }
+  if (localStorage.length > 1) {
+    document.getElementById("gif-count").innerHTML = `(${localStorage.length -
+      1})`;
+  }
 }
 
 function setThemeLS() {
-    theme = document.getElementById("theme").getAttribute('href');
-    if (theme == sailor_day) {
-        localStorage.setItem('theme', 1);
-    } else {
-        localStorage.setItem('theme', 2);
-    }
+  theme = document.getElementById("theme").getAttribute("href");
+  if (theme == sailor_day) {
+    localStorage.setItem("theme", 1);
+  } else {
+    localStorage.setItem("theme", 2);
+  }
 }
 
-window.onload = function () {
+window.onload = function() {
+  // Si alguna vez se predefinió el tema oscuro, permanece el tema por el value en LS
+  if (localStorage.getItem("theme") == 2) {
+    document.getElementById("theme").href = "/gifOS/styles/sailor_night.css";
+    document.getElementById("img-header").src =
+      "/gifOS/images/gifOF_logo_dark.png";
+  }
 
-    // Si alguna vez se predefinió el tema oscuro, permanece el tema por el value en LS
-    if (localStorage.getItem('theme') == 2) {
-        document.getElementById("theme").href = '/gifOS/styles/sailor_night.css';
-        document.getElementById("img-header").src = "/gifOS/images/gifOF_logo_dark.png";
-    }
+  $("#rnd").append(Math.floor(Math.random() * 10000000));
 
-    $("#rnd").append(Math.floor(Math.random() * 10000000));
+  trendingGifs();
+  showLsItems();
+  setThemeLS();
 
-    trendingGifs();
-    showLsItems();
-    setThemeLS();
-
-    $(function () {
-        $(".search-bar input").keypress(function (e) {
-            if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
-                $('.btn-search').click();
-                return false;
-            } else {
-                return true;
-            }
-        });
+  $(function() {
+    $(".search-bar input").keypress(function(e) {
+      if ((e.which && e.which == 13) || (e.keyCode && e.keyCode == 13)) {
+        $(".btn-search").click();
+        return false;
+      } else {
+        return true;
+      }
     });
-}
-
+  });
+};
