3354f47a1f34ac7f85e500a666f0686804369af4
ivancanga <55848546+ivancanga@users.noreply.github.com>
2020-01-29 16:14:19 -0300

    Style modifications

--- scripts/social-media.js
+++ scripts/social-media.js
@@ -18,24 +18,30 @@ function createModal(src) {
           <span>Compartir gif!</span><span style='float: right;'><img id='close' src='/gifOS/images/button3.svg' /></span>
     </div>
     <img class='modal-gif-img' src='${src}'/>
-    <div class='social-media'>
-      <p>
-        <a target='_blank' href='https://wa.me/?text=${src}'>
-          <img src='/gifOS/images/whatsapp.svg'>Enviar como mensaje
-        </a>
-      </p>
 
-      <p>
-        <a target='_blank' href='http://www.facebook.com/sharer/sharer.php?u=${src}'>
-          <img src='/gifOS/images/facebook.svg'>Compartir en Facebook
-        </a>
-      </p>
+    <div class='share-box-flex'>
+      <div class='social-media'>
+        <p>
+          <a target='_blank' href='https://wa.me/?text=${src}'>
+            <img src='/gifOS/images/whatsapp.svg'>Enviar como mensaje
+          </a>
+        </p>
 
-      <p>
-        <a target='_blank' href='http://twitter.com/share?url=${src}'>
-          <img src='/gifOS/images/twitter.svg'>Twittear gif
-        </a>
-      </p>
+        <p>
+          <a target='_blank' href='http://www.facebook.com/sharer/sharer.php?u=${src}'>
+            <img src='/gifOS/images/facebook.svg'>Compartir en Facebook
+          </a>
+        </p>
+        <p>
+          <a target='_blank' href='http://twitter.com/share?url=${src}'>
+            <img src='/gifOS/images/twitter.svg'>Twittear gif
+          </a>
+        </p>
+      </div>
+
+      <div class='download-btn'>
+        <p> <img src='/gifOS/images/folder.svg'></p>
+      </div>
     </div>
   `;
     document.body.appendChild(modalDiv);
